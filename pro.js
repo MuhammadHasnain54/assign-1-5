364e5b9c093f8d769d1e986786ff7435fd835abc
Muhammad Hasnain <154057614+MuhammadHasnain54@users.noreply.github.com>
2024-05-20 18:37:46 +0500

    asign 1 - 5

---
+++ pro.js
@@ -0,0 +1,63 @@
+  // Chapter 1 - 4
+
+  let Name = "Hello ! My Name iS Hasnian";
+  let age = "My Age is 20";
+  let work = "web and app develper";
+ let p =  "Pizza \n Pizz \n Piz \n Pi \n P";
+ 
+  alert(p);
+  
+  alert(Name);
+  alert(age);
+  alert(work);
+
+let  name= prompt('Enter Your Name');
+let  Age= prompt('Enter Your Age');
+let  course= prompt('Enter Your Course');
+alert("NAME: " + name + "\n Age: " + Age + "\n Course " + course);
+
+
+
+let  emailtext= "Your Email Address is ";
+let email= "example@gmail.com";
+alert(emailtext + email);
+
+let  book= 'A Smarter Way Of JS';
+alert("I Am Trying To Learn JS From Book " + book);
+
+document.write("Hn Bi Kaisi Lagi");
+
+let emo= '▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬';
+console.log(emo);
+alert(emo);
+
+
+document.write("<br>" + "For Legal Variable It Must Begin with letter , contain camelcase , contain only $ and _  symbols.");
+document.write("<br>" + "For Illegal Variable It cannot start with number , cannot contain symbols , cannot contain reserved keywords");
+
+document.write("<br>" + "Contain only letter, and doesnot start with number and used only special symbols like only $ and _");
+
+document.write("<br>" + "for answer b blanks:"+"<br>"+"For Legal Variable It Must Begin with letter , camelcase , and only $ and _  symbols.");
+
+document.write("<br>" + "for answer c blanks:" + "<br>" + "Variables must begin with a  letter , $ or _.");
+
+document.write("<br>" + "for answer d blanks: " + " Sensitive" );
+
+document.write(`<br>for answer e blanks:  Keywords<br>` );
+
+// Chapter 5
+
+let  a= prompt("Enter Your Number");
+let  b= prompt("Enter Your Number");
+let  Result = a + b;
+document.write(  "Addition: " + Result + "<br>");
+
+var result = a - b;
+document.write("Subtraction: " + result + "<br>");
+
+var rResult = a * b;
+document.write("Multiplication: " + rResult + "<br>");
+
+var dResult = a / b;
+document.write("Divide: " + dResult + "<br>");
+  
